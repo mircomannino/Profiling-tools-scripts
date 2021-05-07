bd3eb8ea55defbcbb32151beb0cc95d454407a96
Mirco Mannino <mircomannino1997@gmail.com>
2021-05-07 16:37:06 +0200

    Add output dir

--- Perf/perf_report_generator.sh
+++ Perf/perf_report_generator.sh
@@ -3,8 +3,10 @@
 #   $1: binary file to profile
 #   $2: output directory
 
+OUTPUT_DIR=./perf_results
+
 EVENTS_TO_ANALYZE=("cache-misses")
 FILE_NAME=$(basename $1)
 
-mkdir -p $2
-sudo perf stat -e ${EVENTS_TO_ANALYZE} $1 2> $2/${FILE_NAME}
+mkdir -p ${OUTPUT_DIR}
+sudo perf stat -e ${EVENTS_TO_ANALYZE} $1 2> ${OUTPUT_DIR}/${FILE_NAME}

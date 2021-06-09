01194b04f876a0d987cd5453bb8e7462e7393519
Mirco Mannino <mircomannino1997@gmail.com>
2021-06-09 09:30:33 +0200

    Update run_analysis.sh with exectution_time

--- GeneralPurposes/run_analysis.sh
+++ GeneralPurposes/run_analysis.sh
@@ -4,8 +4,9 @@ declare -a list_of_analysis=("1")
 
 declare -a n_repetitions=("1" "50")
 
-declare -a arguments=(
-    "10 3 5 16"     # (Image size, Image depth, Kernel size, N. Kernel)
+declare -a arguments=( # (Image size, Image depth, Kernel size, N. Kernel)
+    "10 3 5 16"
+    "50 40 4 4"
 )
 
 BINARY_FILE=./bin/benchmark_Naive
@@ -15,10 +16,15 @@ do
     CURRENT_DIR=analysis_N${NUMBER_ANALYSYS}
     LOOP_ORDER=${NUMBER_ANALYSYS}
     for ARGUMENT in "${arguments[@]}"
-    do 
+    do
         for N_REPETITIONS in "${n_repetitions[@]}"
-        do  
-            # Perf 
+        do
+            # Execution time
+            OUT_DIR_TIME=${CURRENT_DIR}/ExecutionTime_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_times
+            ./analysis_N${NUMBER_ANALYSYS}/ExecutionTime_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_time_generator.sh \
+            ${BINARY_FILE} ${OUT_DIR_TIME} ${ARGUMENT} ${LOOP_ORDER} ${N_REPETITIONS}
+
+            # Perf
             OUT_DIR_PERF=${CURRENT_DIR}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_reports
             ./analysis_N${NUMBER_ANALYSYS}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_report_generator.sh \
             ${BINARY_FILE} ${OUT_DIR_PERF} ${ARGUMENT} ${LOOP_ORDER} ${N_REPETITIONS}

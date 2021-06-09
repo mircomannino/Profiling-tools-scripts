85410e2d3b623e6e59b994b60bf3b3cdd91296e2
Mirco Mannino <mircomannino1997@gmail.com>
2021-06-09 16:18:27 +0200

    Remove permission settings from perf_report_generator.sh

--- Perf/perf_report_generator.sh
+++ Perf/perf_report_generator.sh
@@ -18,8 +18,8 @@ if [ "$#" -ne 8 ]; then
 fi
 
 # Permissions
-echo "0" | sudo tee /proc/sys/kernel/kptr_restrict
-echo "0" | sudo tee /proc/sys/kernel/perf_event_paranoid
+# echo "0" | sudo tee /proc/sys/kernel/kptr_restrict
+# echo "0" | sudo tee /proc/sys/kernel/perf_event_paranoid
 
 OUTPUT_DIR=$2
 

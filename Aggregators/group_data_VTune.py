8f3fbf7084f119f7a785a1bce9e40f3f4506083a
Mirco Mannino <mircomannino1997@gmail.com>
2021-07-29 16:03:02 +0200

    Add core-bound metric to VTune aggregator

--- Aggregators/group_data_VTune.py
+++ Aggregators/group_data_VTune.py
@@ -87,6 +87,8 @@ class AggregatorVTuneData:
                                 self.results[subdirectory]['CYCLES-3+-PORTS-UTILIZED'] = self.__to_float(line[2])
                             if(metric_name.find('FB Full') != -1):
                                 self.results[subdirectory]['FB-FILL'] = self.__to_float(line[2])
+                            if(metric_name.find('Core Bound') != -1):
+                                self.results[subdirectory]['CORE-BOUND'] = self.__to_float(line[2])
 
                         ### MEMORY ACCESS ###
                         if(parameter_file == 'summary_memory-access.csv'):

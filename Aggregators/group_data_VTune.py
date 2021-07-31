316025f602ecbae63cab9fea05d0d34abebe66fa
Mirco Mannino <mircomannino1997@gmail.com>
2021-07-31 23:09:38 +0200

    Fix DLTB-OVERHEAD metric

--- Aggregators/group_data_VTune.py
+++ Aggregators/group_data_VTune.py
@@ -89,7 +89,7 @@ class AggregatorVTuneData:
                                 self.results[subdirectory]['FB-FILL'] = self.__to_float(line[2])
                             if(metric_name.find('Core Bound') != -1):
                                 self.results[subdirectory]['CORE-BOUND'] = self.__to_float(line[2])
-                            if(metric_name.find('DTLB Overhead' != -1)):
+                            if(metric_name.find('DTLB Overhead') != -1):
                                 self.results[subdirectory]['DTLB-OVERHEAD'] = self.__to_float(line[2])
 
                         ### MEMORY ACCESS ###

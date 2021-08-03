1acd4b38c3047c497944cc7275f2ad24f2298270
Mirco Mannino <mircomannino1997@gmail.com>
2021-08-03 09:12:40 +0200

    Add multiple runs to VTune analyzer

--- Intel-VTune/VTune_report_generator.sh
+++ Intel-VTune/VTune_report_generator.sh
@@ -52,12 +52,12 @@ declare -a collect_types=(
 for TYPE in "${collect_types[@]}"
 do
         mkdir -p ${BIN_VTUNE_DIR}/${BIN_IDENTIFIER}_${TYPE}
-        vtune -collect $TYPE -result-dir ${BIN_VTUNE_DIR}/${BIN_IDENTIFIER}_${TYPE} $1 $4 $5 $6 $7 $8 $9
+        vtune -collect $TYPE -result-dir ${BIN_VTUNE_DIR}/${BIN_IDENTIFIER}_${TYPE} -allow-multiple-runs $1 $4 $5 $6 $7 $8 $9
 done
 
 # Create reports with vtune command
 for TYPE in "${collect_types[@]}"
 do
         vtune -report summary -result-dir ${BIN_VTUNE_DIR}/${BIN_IDENTIFIER}_${TYPE} -format ${FORMAT} -report-output ${BIN_REPORTS_DIR}/summary_${TYPE}.${FORMAT}
-
+        
 done

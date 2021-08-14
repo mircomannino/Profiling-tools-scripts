b0fd1ee08c401e48bd94fd9d8b100c3f354873d3
Mirco Mannino <mircomannino1997@gmail.com>
2021-08-14 11:00:33 +0200

    Add general purposes scripts to handling memory blocking analysis

---
+++ GeneralPurposes/make_folders_memBlocking.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+declare -a list_of_analysis=("1" "2" "3" "4" "5" "6" "7")
+
+declare -a n_repetitions=("5")
+
+# Clone the scripts file for profiling
+rm -rf Profiling-tools-scripts
+git clone https://github.com/mircomannino/Profiling-tools-scripts.git
+
+for NUMBER in "${list_of_analysis[@]}"
+do
+    CURRENT_DIR=analysis_N${NUMBER}
+    # Create the folder
+    mkdir ${CURRENT_DIR}
+    cd ${CURRENT_DIR}
+    # Create a folder for each type of profiling analysis
+    for N_REPETITIONS in "${n_repetitions[@]}"
+    do
+        ### Make folders ###
+        mkdir ExecutionTime_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions    # Execution time
+        mkdir Perf_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions             # Perf
+        mkdir VTune_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions            # VTune
+
+        ### Copy the profiling scripts in the right folder ###
+        cp ../Profiling-tools-scripts/ExecutionTime/execution_time_generator.sh ./ExecutionTime_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions
+        cp ../Profiling-tools-scripts/Perf/perf_report_generator.sh ./Perf_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions
+        cp ../Profiling-tools-scripts/Intel-VTune/VTune_report_generator.sh ./VTune_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions
+
+        ### Permissions ###
+        chmod u+x ./ExecutionTime_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions/*.sh
+        chmod u+x ./Perf_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions/*.sh
+        chmod u+x ./VTune_analysis_N${NUMBER}_${N_REPETITIONS}-repetitions/*.sh
+
+    done
+    # Return to root folder
+    cd ..
+done
+
+# Delete the scripts file for profiling
+rm -rf Profiling-tools-scripts
---
+++ GeneralPurposes/run_analysis_memBlocking.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+declare -a list_of_analysis=("1" "2" "3" "4" "5" "6" "7")
+
+declare -a n_repetitions=("5")
+
+declare -a blocking_size=(
+    "-1 -1 -1"
+    "8 8 8"
+    "16 16 16"
+    "32 32 32"
+)
+
+declare -a arguments=( # (Image size, Image depth, Kernel size, N. Kernel, Cib size, Cob size, Wob size)
+    "227 3 11 96"
+    "27 96 5 256"
+    "13 384 3 384"
+    "13 384 3 256"
+    "13 256 3 384"
+)
+
+BINARY_FILE_NAIVE=./bin/benchmark_Naive
+BINARY_FILE_MEMBLOCKING=./bin/benchmark_MemoryBlocking
+
+for NUMBER_ANALYSYS in "${list_of_analysis[@]}"
+do
+    CURRENT_DIR=analysis_N${NUMBER_ANALYSYS}
+    LOOP_ORDER=${NUMBER_ANALYSYS}
+    for ARGUMENT in "${arguments[@]}"
+    do
+
+        for N_REPETITIONS in "${n_repetitions[@]}"
+        do
+            ##### Naive #####
+            # Execution time
+            OUT_DIR_TIME=${CURRENT_DIR}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_times
+            ./analysis_N${NUMBER_ANALYSYS}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_time_generator.sh \
+            ${BINARY_FILE_NAIVE} ${OUT_DIR_TIME} ${ARGUMENT} ${LOOP_ORDER} ${N_REPETITIONS}
+
+            # Perf
+            OUT_DIR_PERF=${CURRENT_DIR}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_reports
+            ./analysis_N${NUMBER_ANALYSYS}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_report_generator.sh \
+            ${BINARY_FILE_NAIVE} ${OUT_DIR_PERF} ${ARGUMENT} ${LOOP_ORDER} ${N_REPETITIONS}
+
+            # VTune
+            ROOT_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/vtune_data
+            OUT_DIR_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/reports
+            ./analysis_N${NUMBER_ANALYSYS}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/VTune_report_generator.sh \
+            ${BINARY_FILE_NAIVE} ${ROOT_VTUNE} ${OUT_DIR_VTUNE} ${ARGUMENT} ${LOOP_ORDER} ${N_REPETITIONS}
+            rm -rf ${ROOT_VTUNE}/* # Clean tmp data
+
+            ##### Memory Blocking #####
+            for BLOCKING_SIZE in "${blocking_size[@]}"
+            do
+                # Execution time
+                OUT_DIR_TIME=${CURRENT_DIR}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_times
+                ./analysis_N${NUMBER_ANALYSYS}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_time_generator.sh \
+                ${BINARY_FILE_MEMBLOCKING} ${OUT_DIR_TIME} ${ARGUMENT} ${BLOCKING_SIZE} ${LOOP_ORDER} ${N_REPETITIONS}
+
+                # Perf
+                OUT_DIR_PERF=${CURRENT_DIR}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_reports
+                ./analysis_N${NUMBER_ANALYSYS}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_report_generator.sh \
+                ${BINARY_FILE_MEMBLOCKING} ${OUT_DIR_PERF} ${ARGUMENT} ${BLOCKING_SIZE} ${LOOP_ORDER} ${N_REPETITIONS}
+
+                # VTune
+                ROOT_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/vtune_data
+                OUT_DIR_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/reports
+                ./analysis_N${NUMBER_ANALYSYS}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/VTune_report_generator.sh \
+                ${BINARY_FILE_MEMBLOCKING} ${ROOT_VTUNE} ${OUT_DIR_VTUNE} ${ARGUMENT} ${BLOCKING_SIZE} ${LOOP_ORDER} ${N_REPETITIONS}
+                rm -rf ${ROOT_VTUNE}/* # Clean tmp data
+
+            done
+
+        done
+
+    done
+done

ca95c173b47e61ca73732719f3633401d52b0c08
Mirco Mannino <mircomannino1997@gmail.com>
2021-11-16 11:28:50 +0100

    Update execution time script according to parallel version

--- ExecutionTime/execution_time_generator.sh
+++ ExecutionTime/execution_time_generator.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# Arguments:
-#   $1: binary file to profile
-#   $2: output directory
-#   $3: Size of image
-#   $4: Depth of image
-#   $5: Size of kernels
-#   $6: Number of kernels
-#   $7: Blocked input channel size
-#   $8: Blocked output channel size
-#   $9: Blocked output width size
-#   $10[$7]: Order of for loops
-#   $11[$8]: Number of tests to do
+# Arguments table
+# =====================================================================================
+# Arg   |       Naive           | MemoryBlocking                | Parallel            |
+# =====================================================================================
+# $1    |                   binary file to profile                                    |
+# $2    |                   output directory                                          |
+# $3    |                   Size of the image (H and W)                               |
+# $4    |                   Depth of the image                                        |
+# $5    |                   Size of the kernel (H and W)                              |
+# $6    |                   Depth of the kernel                                       |
+# $7    |   Order of loops      | Blocked input channel size    |   Number of threads |
+# $8    |   Number of tests     | Blocked output channel size   |   Order of loops    |
+# $9    |                       | Blocked output width size     |   Number of tests   |
+# $10   |                       | Order of loopa                |                     |
+# $11   |                       | Number of tests                                     |
+# =====================================================================================
 
 if [[ ${BINARY_FILE} =~ "./bin/benchmark_Naive" ]] && [[ "$#" -ne 8 ]]; then
     echo "Insert the following arguments:"
@@ -26,6 +30,12 @@ if [[ ${BINARY_FILE} =~ "./bin/benchmark_MemoryBlocking" ]] && [[ "$#" -ne 11 ]]
     echo "  3-11) Arguments of binary file. See documentation"
     exit 1
 fi
+if [[ ${BINARY_FILE} =~ "./bin/benchmark_Parallel" ]] && [[ "$#" -ne 9 ]]; then
+    echo "Insert the following arguments:"
+    echo "  1) Binary file to analyze"
+    echo "  2) Output directory"
+    echo "  3-9) Arguements of binary file. See documentation"
+fi
 
 
 BINARY_FILE=$1
@@ -37,7 +47,9 @@ fi
 if [[ ${BINARY_FILE} =~ "./bin/benchmark_MemoryBlocking" ]]; then # MemoryBlocking
     FILE_NAME=$(basename $1)_$3_$4_$5_$6_$7_$8_$9_${10}_${11}.txt
 fi
-
+if [[ ${BINARY_FILE} =~ "./bin/benchmark_Parallel" ]]; then # Parallel
+    FILE_NAME=$(basename $1)_$3_$4_$5_$6_$7_$8_$9.txt
+fi
 
 mkdir -p ${OUTPUT_DIR}
 if [[ ${BINARY_FILE} =~ "./bin/benchmark_Naive" ]]; then
@@ -52,3 +64,5 @@ fi
 if [[ ${BINARY_FILE} =~ "./bin/benchmark_MemoryBlocking" ]]; then
     ${BINARY_FILE} $3 $4 $5 $6 $7 $8 $9 ${10} ${11} | tee ${OUTPUT_DIR}/${FILE_NAME}
 fi
+if [[ ${BINARY_FILE} =~ "./bin/benchmark_Parallel" ]]; then
+    ${BINARY_FILE} $3 $4 $5 $6 $7 $8 $9 | tee ${OUTPUT_DIR}/${FILE_NAME}

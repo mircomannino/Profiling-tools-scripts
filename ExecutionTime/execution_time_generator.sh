b3f6efee59dda55c8220dac62359f4d0d28e673d
Mirco Mannino <mircomannino1997@gmail.com>
2021-11-19 12:17:55 +0100

    Update if statement in execution_time_generator.sh

--- ExecutionTime/execution_time_generator.sh
+++ ExecutionTime/execution_time_generator.sh
@@ -21,7 +21,7 @@
 BINARY_FILE=$1
 OUTPUT_DIR=$2
 
-if [[ ${BINARY_FILE} =~ "./bin/benchmark_Complete" ]] && [[ "$#" -ne 12 ]]; then
+if [[ ${BINARY_FILE} =~ "./bin/benchmark_ParallelMemoryBlocking" ]] && [[ "$#" -ne 12 ]]; then
     echo "Detect Parallel + Memory blocking version with wrong arguments"
     echo "Insert the following arguments:"
     echo "  1) Binary file to analyze"
@@ -29,7 +29,7 @@ if [[ ${BINARY_FILE} =~ "./bin/benchmark_Complete" ]] && [[ "$#" -ne 12 ]]; then
     echo "  3-12) Arguements of binary file. See documentation"
     exit 1
 elif [[ ${BINARY_FILE} =~ "./bin/benchmark_Parallel" ]] && [[ "$#" -ne 9 ]]; then
-    echo "Detect Parallel version with wrong arguments"   
+    echo "Detect Parallel version with wrong arguments"
     echo "Insert the following arguments:"
     echo "  1) Binary file to analyze"
     echo "  2) Output directory"

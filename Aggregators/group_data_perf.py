7ba33cb6e441d0d81be40fe426d27855f76f4069
Mirco Mannino <mircomannino1997@gmail.com>
2022-01-02 14:14:00 +0100

    Fix key name in results dict in perf_report_generator.sh

--- Aggregators/group_data_perf.py
+++ Aggregators/group_data_perf.py
@@ -64,8 +64,8 @@ class AggregatorPerfData:
                         self.results[test_file_name_key]['N-128b-PACKED-SINGLE'] = self.__get_128b_packed_single(line)
                     if(line.find('fp_arith_inst_retired.256b_packed_single') != -1):
                         self.results[test_file_name_key]['N-256b-PACKED-SINGLE'] = self.__get_256b_packed_single(line)
-                self.results[test_file_name_key]['N-256b-PACKED-SINGLE-OVER-N-INSTRUCTIONS'] = float(self.results[test_file_name_key]['N-256b-PACKED-SINGLE'] / self.results[test_file_name]['N-INSTRUCTIONS'])
-                self.results[test_file_name_key]['CACHE-OVER-INSTRUCTIONS'] = float(self.results[test_file_name_key]['CACHE-MISSES-NUMBER']) / float(self.results[test_file_name]['N-INSTRUCTIONS'])
+                self.results[test_file_name_key]['N-256b-PACKED-SINGLE-OVER-N-INSTRUCTIONS'] = float(self.results[test_file_name_key]['N-256b-PACKED-SINGLE'] / self.results[test_file_name_key]['N-INSTRUCTIONS'])
+                self.results[test_file_name_key]['CACHE-OVER-INSTRUCTIONS'] = float(self.results[test_file_name_key]['CACHE-MISSES-NUMBER']) / float(self.results[test_file_name_key]['N-INSTRUCTIONS'])
         # Show the final collected data
         print('Data grouped!')
         for file_name, parameters in self.results.items():

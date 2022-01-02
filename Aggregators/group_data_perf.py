ee4e66f1eb3e1cd82e8d1183321935f773b03b38
Mirco Mannino <mircomannino1997@gmail.com>
2022-01-02 16:26:30 +0100

    Fix key name in results dict in perf_report_generator.sh

--- Aggregators/group_data_perf.py
+++ Aggregators/group_data_perf.py
@@ -50,9 +50,6 @@ class AggregatorPerfData:
             else:
                 analysis_files.append(test_file_name_key)
 
-            print(analysis_files)
-            ss = input('-----')
-
             self.results[test_file_name_key] = {}
 
             for analysis_file in analysis_files:

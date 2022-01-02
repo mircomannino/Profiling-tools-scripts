a32c5971f52e0f35d8411b9bc984cb6f73006897
Mirco Mannino <mircomannino1997@gmail.com>
2022-01-02 14:02:34 +0100

    Fix key name in results dict in perf_report_generator.sh

--- Aggregators/group_data_perf.py
+++ Aggregators/group_data_perf.py
@@ -35,9 +35,7 @@ class AggregatorPerfData:
         # Iterate all the file of the current folder
         for test_file_name in file_in_folder:
             # Remove event suffix
-            test_file_name_key = test_file_name.replace('_memory','')
-            test_file_name_key = test_file_name_key.replace('_generalPurpose','')
-            print(test_file_name, test_file_name_key, '\n\n\n\n')
+            test_file_name_key = test_file_name.replace('_memory','').replace('_generalPurpose','')
             self.results[test_file_name_key] = {}
             with open(os.path.join(os.getcwd(), test_file_name)) as test_file:
                 for line in test_file:

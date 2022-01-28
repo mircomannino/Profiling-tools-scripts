8a266f6d3990ad8ad480a80ac5a38a4fe46b96e6
Mirco Mannino <mircomannino1997@gmail.com>
2022-01-28 14:08:31 +0100

    Add delay in  thread overhead empty  test

--- GeneralPurposes/run_analysis_threadOverheadEmpty.sh
+++ GeneralPurposes/run_analysis_threadOverheadEmpty.sh
@@ -6,7 +6,7 @@ declare -a n_threads=("1" "2" "3" "4" "5" "6" "7" "8")
 BINARY_FILE=./bin/test_threadOverheadEmpty
 
 OUTPUT_DIR=./results
-mkdir -p OUTPUT_DIR
+mkdir -p ${OUTPUT_DIR}
 
 for N_THREADS in "${n_threads[@]}"
 do
@@ -14,4 +14,6 @@ do
     OUTPUT_NAME=$(basename ${BINARY_FILE})_${N_THREADS}_${N_REPETITIONS}.txt
     # Execute the test and redirect output in a file
     ${BINARY_FILE} ${N_THREADS} ${N_REPETITIONS} | tee ${OUTPUT_DIR}/${OUTPUT_NAME}
+    # Sleep 1 second before next run
+    sleep 1
 done

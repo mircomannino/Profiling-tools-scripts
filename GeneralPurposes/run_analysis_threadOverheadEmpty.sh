b5263ee759d2a417b71e57f8d1ac849bf0d21452
Mirco Mannino <mircomannino1997@gmail.com>
2022-01-28 14:00:42 +0100

    Add new run_analysis for thread test overhead empty

---
+++ GeneralPurposes/run_analysis_threadOverheadEmpty.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+N_REPETITIONS=50
+declare -a n_threads=("1" "2" "3" "4" "5" "6" "7" "8")
+
+BINARY_FILE=./bin/test_threadOverheadEmpty
+OUTPUT_DIR=./results
+
+for N_THREADS in "${n_threads[@]}"
+do
+    # Create output name
+    OUTPUT_NAME=$(basename ${BINARY_FILE})_${N_THREADS}_${N_REPETITIONS}.txt
+    # Execute the test and redirect output in a file
+    ${BINARY_FILE} ${N_THREADS} ${N_REPETITIONS} | tee ${OUTPUT_DIR}/${OUTPUT_NAME}
+done

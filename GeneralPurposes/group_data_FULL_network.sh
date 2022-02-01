f6bbc80b9c821d7237e13bb08a6cede300619a5a
Mirco Mannino <mircomannino1997@gmail.com>
2022-02-01 17:04:13 +0000

    Add group_data_FULL_network.sh script

---
+++ GeneralPurposes/group_data_FULL_network.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Download repository with aggregators scripts
+rm -rf Profiling-tools-scripts
+git clone https://github.com/mircomannino/Profiling-tools-scripts.git
+
+ROOT_FOLDER=$(pwd)
+ALL_ROOFLINES_FOLDER=$(pwd)/roofline-models
+
+for ANALYSIS_FOLDER in ./analysis_N*
+do
+    cd ${ANALYSIS_FOLDER}
+
+    # Execution time
+    for EXECUTION_TIME_FOLDER in ./ExecutionTime*
+    do
+        cd ${EXECUTION_TIME_FOLDER}
+        CSV_NAME=$(basename `pwd`)
+        FULL_NETWORK='--full-network'
+        # Copy the python script to group data
+        cp ${ROOT_FOLDER}/Profiling-tools-scripts/Aggregators/group_execution_times.py ./execution_times/group_execution_times.py
+        # Exectue the python script to group data
+        cd ./execution_times
+        python3 group_execution_times.py -o ${CSV_NAME} ${FULL_NETWORK}
+        # Delete python script
+        rm group_execution_times.py
+        cd ../..
+    done
+
+    cd ..
+done
+
+# Remove folder with grouping scripts
+rm -rf Profiling-tools-scripts

ef5932735ce8413f3eb1ec01d31e3ef1e68110ea
Mirco Mannino <mircomannino1997@gmail.com>
2022-02-02 11:53:23 +0000

    Update output name of alloc_core_type

--- ExecutionTime/execution_time_generator.sh
+++ ExecutionTime/execution_time_generator.sh
@@ -62,7 +62,7 @@ fi
 # Setup output folder and arguments
 if [[ ${BINARY_FILE} =~ ./bin/benchmark_Parallel[a-zA-Z]+FULL$ ]]; then
     OUT_FILE_NAME=$(basename $1)_$3_$4_$5_$6.txt
-    ARGUMENTS="$3 $4 $5 $6"
+    ARGUMENTS="$3 $4 $5 $($6//[_]/-)"
 elif [[ ${BINARY_FILE} =~ "./bin/benchmark_ParallelMemoryBlocking" ]]; then # Parallel + Memory blocking
     OUT_FILE_NAME=$(basename $1)_$3_$4_$5_$6_$7_$8_$9_${10}_${11}_${12}.txt
     ARGUMENTS="$3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}"

35f5ba21c410806005f914f25bd093c02451c131
Mirco Mannino <mircomannino1997@gmail.com>
2022-02-15 16:33:21 +0000

    Debug

--- GeneralPurposes/run_analysis_parallel_memoryUsage.sh
+++ GeneralPurposes/run_analysis_parallel_memoryUsage.sh
@@ -15,29 +15,29 @@ for NUMBER_ANALYSYS in "${list_of_analysis[@]}"
 do
     CURRENT_DIR=analysis_N${NUMBER_ANALYSYS}
     LOOP_ORDER=${NUMBER_ANALYSYS}
-
-        for N_REPETITIONS in "${n_repetitions[@]}"
+    
+    for N_REPETITIONS in "${n_repetitions[@]}"
+    do
+        ##### Parallel #####
+        for N_THREADS in "${n_threads[@]}"
         do
-            ##### Parallel #####
-            for N_THREADS in "${n_threads[@]}"
-            do
-                # # Execution time
-                # OUT_DIR_TIME=${CURRENT_DIR}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_times
-                # ./analysis_N${NUMBER_ANALYSYS}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_time_generator.sh \
-                # ${BINARY_FILE_PARALLEL} ${OUT_DIR_TIME} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS}
+            # # Execution time
+            # OUT_DIR_TIME=${CURRENT_DIR}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_times
+            # ./analysis_N${NUMBER_ANALYSYS}/ExecutionTime_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/execution_time_generator.sh \
+            # ${BINARY_FILE_PARALLEL} ${OUT_DIR_TIME} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS}
 
-                # # Perf
-                # OUT_DIR_PERF=${CURRENT_DIR}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_reports
-                # ./analysis_N${NUMBER_ANALYSYS}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_report_generator.sh \
-                # ${BINARY_FILE_PARALLEL} ${OUT_DIR_PERF} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS}
+            # # Perf
+            # OUT_DIR_PERF=${CURRENT_DIR}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_reports
+            # ./analysis_N${NUMBER_ANALYSYS}/Perf_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/perf_report_generator.sh \
+            # ${BINARY_FILE_PARALLEL} ${OUT_DIR_PERF} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS}
 
-                # VTune
-                ROOT_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/vtune_data
-                OUT_DIR_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/reports
-                ./analysis_N${NUMBER_ANALYSYS}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/VTune_report_generator.sh \
-                ${BINARY_FILE_PARALLEL} ${ROOT_VTUNE} ${OUT_DIR_VTUNE} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS} ${CORE_ALLOCATION_TYPE}
-                rm -rf ${ROOT_VTUNE}/* # Clean tmp data
+            # VTune
+            ROOT_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/vtune_data
+            OUT_DIR_VTUNE=${CURRENT_DIR}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/reports
+            ./analysis_N${NUMBER_ANALYSYS}/VTune_analysis_N${NUMBER_ANALYSYS}_${N_REPETITIONS}-repetitions/VTune_report_generator.sh \
+            ${BINARY_FILE_PARALLEL} ${ROOT_VTUNE} ${OUT_DIR_VTUNE} ${ARGUMENT} ${N_THREADS} ${LOOP_ORDER} ${N_REPETITIONS} ${CORE_ALLOCATION_TYPE}
+            rm -rf ${ROOT_VTUNE}/* # Clean tmp data
 
-            done
+        done
     done
 done

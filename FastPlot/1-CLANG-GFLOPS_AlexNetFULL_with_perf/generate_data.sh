74e98c29782f437041b783653db1917787b50109
Mirco Mannino <mircomannino1997@gmail.com>
2022-03-04 17:09:30 +0000

    Clean up

---
+++ FastPlot/1-CLANG-GFLOPS_AlexNetFULL_with_perf/generate_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a n_analysis=("2" "3" "4")
+declare -a n_threads=("1" "2" "3" "4" "5" "6" "7" "8" "9" "9" "10" "11" "12" "13" "14" "15" "16")
+
+PERF_EVENTS=("fp_arith_inst_retired.128b_packed_single,fp_arith_inst_retired.256b_packed_single,fp_arith_inst_retired.scalar_single")
+
+ALLOCATION_TYPE="PHYCORE1_THREAD1"
+CONVOLUTION_N_REPETITIONS=1
+PERF_N_REPETITIONS=100
+
+BIN=./bin/benchmark_ParallelAlexNetFULL
+
+OUT_DIR=./results
+mkdir -p ${OUT_DIR}
+
+for N_ANALYSIS in ${n_analysis[@]}; do 
+    for N_THREADS in ${n_threads[@]}; do
+        OUT_NAME=$(basename ${BIN})_order-N${N_ANALYSIS}_n-repetitions-${N_REPETITIONS}_n-threads-${N_THREADS}
+        
+        ARGUMENTS="${N_THREADS} ${N_ANALYSIS} ${CONVOLUTION_N_REPETITIONS} ${ALLOCATION_TYPE}"
+		echo ${ARGUMENTS}
+        perf stat -o ${OUT_DIR}/${OUT_NAME}.txt -r ${PERF_N_REPETITIONS} -e ${PERF_EVENTS} ${BIN} ${ARGUMENTS}
+    done
+done
---
+++ FastPlot/1-CLANG-GFLOPS_AlexNetFULL_with_perf/peak_interpolation.py
@@ -0,0 +1,93 @@
+# Script used to compute the maximum perforamance peak for a given number of core 
+from configparser import Interpolation
+import numpy as np
+import matplotlib.pyplot as plt
+
+class BlackYeti:
+    def __init__(self):
+        # Initialize with the known bandwidth peaks
+        L1 = { # Max bandwidth [GB/s]
+            '1-cores': 425.28,
+            '2-cores': 851.16,
+            '4-cores': 1702.32,
+            '6-cores': 2553.47,
+            '8-cores': 3404.63
+        }
+        L2 = { # Max bandwidth [GB/s]
+            '1-cores': 150.44,
+            '2-cores': 300.87,
+            '4-cores': 601.74,
+            '6-cores': 902.47,
+            '8-cores': 1203.63
+        }
+        L3 = { # Max bandwidth [GB/s]
+            '1-cores': 67.85,
+            '2-cores': 135.7,
+            '4-cores': 271.39,
+            '6-cores': 407.09,
+            '8-cores': 542.79
+        }
+        DRAM = { # Max bandwidth [GB/s]
+            '1-cores': 23.52,
+            '2-cores': 27.35,
+            '4-cores': 30.86,
+            '6-cores': 31.23,
+            '8-cores': 31.36
+        }
+
+        # Initialize max peaks at 0.16-0.17 FLOPs/Byte (OI)
+        self.max_OI = {}
+        self.max_OI['L1'] = {}
+        self.max_OI['L2'] = {}
+        self.max_OI['L3'] = {}
+        self.max_OI['DRAM'] = {}
+        self.OI = 0.167
+        for n_cores in L1.keys():
+            self.max_OI['L1'][n_cores] = L1[n_cores] * self.OI
+            self.max_OI['L2'][n_cores] = L2[n_cores] * self.OI
+            self.max_OI['L3'][n_cores] = L3[n_cores] * self.OI
+            self.max_OI['DRAM'][n_cores] = DRAM[n_cores] * self.OI
+        
+
+    def L1_interpolation(self, n_cores):
+        x_val = np.array([1, 2, 4, 6, 8]) # Number of cores
+        y_val = np.array([v for k,v in self.max_OI['L1'].items()])
+        return np.interp(n_cores, x_val, y_val)
+    
+    def L2_interpolation(self, n_cores):
+        x_val = np.array([1, 2, 4, 6, 8]) # Number of cores
+        y_val = np.array([v for k,v in self.max_OI['L2'].items()])
+        return np.interp(n_cores, x_val, y_val)
+    
+    def L3_interpolation(self, n_cores):
+        x_val = np.array([1, 2, 4, 6, 8]) # Number of cores
+        y_val = np.array([v for k,v in self.max_OI['L3'].items()])
+        return np.interp(n_cores, x_val, y_val)
+
+    def DRAM_interpolation(self, n_cores):
+        x_val = np.array([1, 2, 4, 6, 8]) # Number of cores
+        y_val = np.array([v for k,v in self.max_OI['DRAM'].items()])
+        return np.interp(n_cores, x_val, y_val)
+
+if __name__=='__main__':
+    b = BlackYeti()
+
+    # Demo of the interpolation
+    x = np.array([1, 2, 4, 6, 8])
+    y_L1 = b.L1_interpolation(x)
+    y_L2 = b.L2_interpolation(x)
+    y_L3 = b.L3_interpolation(x)
+    y_DRAM = b.DRAM_interpolation(x)
+
+    plt.plot(x, y_L1, 'ro-', label='L1')
+    plt.plot(x, y_L2, 'bo-', label='L2')
+    plt.plot(x, y_L3, 'go-', label='L3')
+    plt.plot(x, y_DRAM, 'yo-', label='DRAM')
+
+    plt.grid('y')
+
+    plt.title('Max Performance GFLOPs/S at 0.167 FLOPs/Byte')
+    plt.ylabel('Performance [GFLOPs/S]')
+    plt.xlabel('Number of cores')
+    plt.legend()
+    plt.show()
